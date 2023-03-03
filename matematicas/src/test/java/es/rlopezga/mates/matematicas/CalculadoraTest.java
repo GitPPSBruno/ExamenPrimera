87eddfd2de24045afb8f7c57b23a539bf346e39a
Bruno Lopez Barcia <brunolopar46@gmail.com>
2023-03-03 17:07:25 +0100

    Añadido el proyecto inicial de java

---
+++ matematicas/src/test/java/es/rlopezga/mates/matematicas/CalculadoraTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package es.rlopezga.mates.matematicas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author usuario
+ *
+ */
+class CalculadoraTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
